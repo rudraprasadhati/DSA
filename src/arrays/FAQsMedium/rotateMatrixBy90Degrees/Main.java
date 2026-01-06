668ea0be6fb2cb1b5734c510b3d95c655f2dc6af
Rudra Prasad Hati <rudraprasadhatiofficial@gmail.com>
2026-01-07 00:17:04 +0530

    fix rotateMatrixBy90Degrees

--- src/arrays/FAQsMedium/rotateMatrixBy90Degrees/Main.java
+++ src/arrays/FAQsMedium/rotateMatrixBy90Degrees/Main.java
@@ -2,7 +2,7 @@ package arrays.FAQsMedium.rotateMatrixBy90Degrees;
 
 class Solution {
     //1st we took out the transpose of the matrix and then we reversed it
-    public int[][] rotateMatrix(int[][] arr) {
+    public void rotateMatrix(int[][] arr) {
         //transpose of the matrix
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
@@ -24,17 +24,16 @@ class Solution {
                 arr[i][arr.length-1-j] = temp;
             }
         }
-        return arr;
     }
 }
 public class Main {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         Solution s = new Solution();
-        int[][] result = s.rotateMatrix(arr);
-        for(int i = 0; i<result.length; i++) {
-            for(int j = 0; j<result.length; j++) {
-                System.out.print(result[i][j]+" ");
+        s.rotateMatrix(arr);
+        for(int i = 0; i<arr.length; i++) {
+            for(int j = 0; j<arr.length; j++) {
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
