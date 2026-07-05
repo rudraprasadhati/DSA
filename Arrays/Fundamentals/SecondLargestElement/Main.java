02de1abf1f01938270a201bfad40d197774faff6
rudraprasadhati <rudraprasadhatiofficial@gmail.com>
2026-07-05 21:51:16 +0530

    edit secondLargestElement

--- Arrays/Fundamentals/SecondLargestElement/Main.java
+++ Arrays/Fundamentals/SecondLargestElement/Main.java
@@ -3,6 +3,7 @@ package Arrays.Fundamentals.SecondLargestElement;
 import java.util.*;
 
 class Solution {
+//My approach
 //In this implementation, the code is a bit tricky, and is hard to read compared to the solution below which is not commented.
 //Here the sorting functions takes O(n logn) time, and the while loop takes O(n) time, making the time complexity as: O(n logn)+O(n)
 //As we ignore the insignificant values, the overall time complexity becomes: O(n logn)
@@ -21,6 +22,7 @@ class Solution {
 //        return nums[i];
 //    }
 
+//My approach
 //This is not optimal because it uses a tree-set, which requires a space complexity of O(n).
 //The tree-set insertion also takes O(logn) time, and combined with the for loop, the over time complexity becomes O(n logn).
 //    public int secondLargestElement(int[] nums) {
@@ -35,19 +37,62 @@ class Solution {
 //        return list.get(list.size()-2);
 //    }
 
+//Brute force solution
+//The sorting takes O(n logn), and the for-loop takes O(n) time, making the time complexity: O(n)+O(n logn)
+//Overall time complexity: O(n logn) (as we ignore the insignificant values)
+//    public int secondLargestElement(int[] nums) {
+//        if(nums.length<=1) {
+//            return -1;
+//        }
+//        Arrays.sort(nums);
+//        int largest = nums[nums.length-1];
+//        for(int i = nums.length-2; i>=0; i--) {
+//            if(nums[i]!=largest) {
+//                int secondLargest = nums[i];
+//                return secondLargest;
+//            }
+//        }
+//        return -1;
+//    }
+
+//Better solution
+//Time complexity: O(n+n) = O(2n) = O(n)
+//    public int secondLargestElement(int[] nums) {
+//        if(nums.length<2) {
+//            return -1;
+//        }
+//        int largestElement = Integer.MIN_VALUE;
+//        for(int i = 0; i<nums.length; i++) {
+//            if(nums[i]>largestElement) {
+//                largestElement = nums[i];
+//            }
+//        }
+//        int secondLargest = Integer.MIN_VALUE;
+//        for(int i = 0; i<nums.length; i++) {
+//            if(nums[i]>secondLargest && nums[i]!=largestElement) {
+//                secondLargest = nums[i];
+//            }
+//        }
+//        return secondLargest==Integer.MIN_VALUE ? -1 : secondLargest;
+//    }
+
+//Optimal Solution
     public int secondLargestElement(int[] nums) {
-        if(nums.length<=1) {
+        if(nums.length<2) {
             return -1;
         }
-        Arrays.sort(nums);
-        int largest = nums[nums.length-1];
-        for(int i = nums.length-2; i>=0; i--) {
-            if(nums[i]!=largest) {
-                int secondLargest = nums[i];
-                return secondLargest;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i]>largest) {
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i]<largest && nums[i]>secondLargest) {
+                secondLargest = nums[i];
             }
         }
-        return -1;
+        return secondLargest==Integer.MIN_VALUE ? -1 : secondLargest;
     }
 }
 public class Main {
@@ -66,5 +111,6 @@ public class Main {
     }
 }
 
-//The sorting takes O(n logn), and the for-loop takes O(n) time, making the time complexity: O(n)+O(n logn)
-//Overall time complexity: O(n logn) (as we ignore the insignificant values)
+//Time complexity: O(n)
+
+//Critical zone: Remember to add a check condition for numbers which are smaller than "largest" variable but greater than the "secondLargest" variable.
