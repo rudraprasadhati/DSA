9193042d58843a651389d5c247370dd741ea5b52
rudraprasadhati <rudraprasadhatiofficial@gmail.com>
2026-07-22 23:31:43 +0530

    add nextPermutation

---
+++ Arrays/FAQs_Medium/NextPermutation/Main.java
@@ -0,0 +1,23 @@
+package Arrays.FAQs_Medium.NextPermutation;
+
+class Solution {
+//Brute force solution: the brute force solution is very expensive in terms of time complexity, so we have to just discuss it and tell the idea and intuition behind it.
+//For the brute-force solution, we will just generate all the possible permutations by recursion, and after generating all of the permutations, we can simply run a loop through all the permutation which will give the next permutation of the current permutation.
+//Time complexity: n * n!
+//Space complexity: n * n!
+//We will analyseing the time and space complexity in more detail in the recursion part, where we will b implementing this code.
+
+//Optimal solution
+    public void nextPermutation(int[] nums) {
+
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 3};
+        Solution s = new Solution();
+        s.nextPermutation(arr);
+
+    }
+}
