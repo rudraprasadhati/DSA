c787bd3fd3d86789053cd285d3fb77ca1866997b
rudraprasadhati <rudraprasadhatiofficial@gmail.com>
2026-07-26 23:12:21 +0530

    edit longestConsecutiveSubsequenceInAnArray

--- Hashing/FAQs/LongestConsecutiveSubsequenceInAnArray/Main.java
+++ Hashing/FAQs/LongestConsecutiveSubsequenceInAnArray/Main.java
@@ -3,7 +3,7 @@ package Hashing.FAQs.LongestConsecutiveSubsequenceInAnArray;
 public class Main {
     public static void main(String[] args) {
 
-
+        int[] arr = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
 
     }
 }
