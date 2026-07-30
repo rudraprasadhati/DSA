d6d47493630cc5830e0d600bd02a52b50a60bb9a
rudraprasadhati <rudraprasadhatiofficial@gmail.com>
2026-07-30 11:46:18 +0530

    edit quickSort

--- Sorting/QuickSort/Main.java
+++ Sorting/QuickSort/Main.java
@@ -1,19 +1,27 @@
 package Sorting.QuickSort;
 
+import java.util.Random;
+
 class Solution {
     private int partitionFunction(int left, int right, int[] nums) {
+        Random rand = new Random();
+        int randomIndex = left + rand.nextInt(right - left + 1);
+        int tempNum = nums[left];
+        nums[left] = nums[randomIndex];
+        nums[randomIndex] = tempNum;
+
         int pivot = nums[left];
         int i = left; //We are starting from the "i" from the pivot position itself because as we are using the Hoare's partiton scheme, so this ensures that our variables "i" and "j" cross each other at the right place, and out "j" variable stop at the right place, which is to be swapped with the pivot.
         int j = right;
         while(i<j) {
             //Here we iterate and move the "i" pointer forward until we find the correct element on the left side which is greater than the pivot.
             //"i<=right-1", this line ensures that, we don't go out of the right most index and re-enter the while loop, which would give us an index-out-of-bounds error.
-            while(pivot>=nums[i] && i<=right-1) {
+            while(i<=right-1 && pivot>=nums[i]) {
                 i++;
             }
             //Here we iterate and move the "j" pointer backward until we find the correct element on the right side which is smaller than or equal to the pivot.
             //"j>=left+1", this line ensures that, we don't go out of the left most index and re-enter the while loop, which would give us an index-out-of-bounds error.
-            while(pivot<nums[j] && j>=left+1) {
+            while(j>=left+1 && pivot<nums[j]) {
                 j--;
             }
             //Until we reach this portion of the loop, out pointers should be at the correct elements which are eligible for swapping.
@@ -22,8 +30,6 @@ class Solution {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                i++;
-                j--;
             }
         }
         //At last, until this point, our pointers would have crossed each other, and j should be pointing to the last element of the left side which either smaller or equal to the pivot, so we swap them.
